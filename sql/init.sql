bccc34aad2eea40d64a19a1d95afc8164d99dc42
ai-image-game <image-game.ai@gmail.com>
2024-10-27 11:38:04 +0900

    초기화 SQL문 visible 추가

--- sql/init.sql
+++ sql/init.sql
@@ -1,2 +1,3 @@
 update game_data set `uuid` = UUID();
+update game_data set visible = 1;
 update game_data set pc_image = concat('/images/', pc_image), mobile_image = concat('/images/', mobile_image);
