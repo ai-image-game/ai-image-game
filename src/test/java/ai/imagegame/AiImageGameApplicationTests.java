b681d12e2ce5a579cfd6b209e22b09058b3b4c66
ai-image-game <image-game.ai@gmail.com>
2024-11-15 18:30:13 +0900

    테스트 파일 수정

--- src/test/java/ai/imagegame/AiImageGameApplicationTests.java
+++ src/test/java/ai/imagegame/AiImageGameApplicationTests.java
@@ -1,17 +1,16 @@
 package ai.imagegame;
 
-import ai.imagegame.repository.v1.RedisGameDataV1;
-import ai.imagegame.repository.v1.GameDataEntityV1;
 import ai.imagegame.repository.v1.GameDataEntityRepositoryV1;
+import ai.imagegame.repository.v1.RedisGameDataV1;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.HashOperations;
-
-import java.util.List;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class AiImageGameApplicationTests {
 	@Autowired
 	private GameDataEntityRepositoryV1 gameDataEntityRepositoryV1;
@@ -21,13 +20,13 @@ class AiImageGameApplicationTests {
 
 	@Test
 	void contextLoads() {
-		List<GameDataEntityV1> gameDataList = gameDataEntityRepositoryV1.findAll();
+		/*List<GameDataEntityV1> gameDataList = gameDataEntityRepositoryV1.findAll();
 		gameDataList.forEach(gameData -> {
 			String key = String.join("_", IMAGE_KEY_PREFIX, String.valueOf(gameData.getLevel()));
 			this.hashOperations.put(key, String.valueOf(gameData.getUuid()), new RedisGameDataV1(gameData));
 		});
 		String uuid = gameDataList.get(0).getUuid();
-		System.out.println("#### " +this.hashOperations.randomEntry(String.join("_", IMAGE_KEY_PREFIX, uuid)));
+		System.out.println("#### " +this.hashOperations.randomEntry(String.join("_", IMAGE_KEY_PREFIX, uuid)));*/
 	}
 
 }
